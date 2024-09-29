bdb5a02ef589c0ee881a4697ff5b691e0e2e167a
Long Pham <longpd1911@gmail.com>
2024-09-29 11:10:07 +0700

    first commit

--- Lab01/src/Triangle.java
+++ Lab01/src/Triangle.java
@@ -5,11 +5,11 @@ public class Triangle {
         System.out.print("Enter n: ");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        for (int i = 0; i < n; i++){
-            for (int k = 0; k < n - i - 1; k++){
+        for (int i = 0; i < n; ++i){
+            for (int k = 0; k < n - i - 1; ++k){
                 System.out.print(" ");
             }
-            for (int j = 0; j < 2*i + 1; j++){
+            for (int j = 0; j < 2 * i + 1; ++j){
                 System.out.print("*");
             }
             System.out.print("\n");
